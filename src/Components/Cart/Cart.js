0175a726c745d61ea897685f9298568d518c9888
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-03-23 12:08:16 +0530

    Making Add to Cart Work

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,32 +1,17 @@
+import { useContext } from "react";
+import CartContext from "../../Store/cart-context";
 import Modal from "../../UI/Modal/Modal";
 import classes from './Cart.module.css'
 const CartElem = (props) => {
-  const cartElements = [
-    {
-        title: "Colors",
-        price: 100,
-        imageUrl:
-          "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-          quantity: 2,
-      },
-    {
-      title: "Black and white Colors",
-      price: 50,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-      quantity: 3,
-    },
-    {
-      title: "Yellow and Black Colors",
-      price: 70,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-    quantity: 1,
-    },
-  ];
+    const cartCtx=useContext(CartContext)
+    const addToCartHandler=(clickedprod)=>{
+        cartCtx.addItem({
+            id:props.id,
+        })
+    }
   return (
     <Modal onClose={props.onClose}>
-        <div>Items</div>
+        <div onAddToCart={addToCartHandler}></div>
         <button className={classes.close} onClick={props.onClose}>Close</button>
         <button className={classes.order}>Order</button>
     </Modal>
